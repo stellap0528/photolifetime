758fe889609f0d1a8123ec45efb3d1c1b500e50c
unknown <lewistd@lewistd-w541.rose-hulman.edu>
2018-04-14 01:09:55 -0400

    Trey-initial commit of what I have so far

---
+++ PhotoLifetime/app/src/main/java/edu/rosehulman/lewistd/photolifetime/MainActivity.java
@@ -0,0 +1,56 @@
+package edu.rosehulman.lewistd.photolifetime;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+//    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.photo_button:
+//                    mTextMessage.setText(R.string.photo_nav_text);
+                    dispatchTakePictureIntent();
+                    return true;
+                case R.id.video_button:
+//                    mTextMessage.setText(R.string.video_nav_text);
+                    return true;
+                case R.id.gallery_button:
+//                    mTextMessage.setText(R.string.gallery_nav_text);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private void dispatchTakePictureIntent(){
+        Intent takePictureIntent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(takePictureIntent.resolveActivity(getPackageManager())!=null){
+            startActivityForResult(takePictureIntent,REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+}
