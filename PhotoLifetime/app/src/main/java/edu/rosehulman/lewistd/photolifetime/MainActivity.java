9acb4eb6bc2e6da3ca63eee83965751fd75ee521
parks8 <parks8@rose-hulman.edu>
2018-04-16 14:16:53 -0400

    Implemented gallery

--- PhotoLifetime/app/src/main/java/edu/rosehulman/lewistd/photolifetime/MainActivity.java
+++ PhotoLifetime/app/src/main/java/edu/rosehulman/lewistd/photolifetime/MainActivity.java
@@ -1,6 +1,7 @@
 package edu.rosehulman.lewistd.photolifetime;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
@@ -12,7 +13,8 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
-
+    private static final int PICK_IMAGE =100;
+    Uri imageUri;
 //    private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -30,11 +32,14 @@ public class MainActivity extends AppCompatActivity {
                     return true;
                 case R.id.gallery_button:
 //                    mTextMessage.setText(R.string.gallery_nav_text);
+                    openGallery();
                     return true;
             }
             return false;
         }
     };
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +51,18 @@ public class MainActivity extends AppCompatActivity {
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK && requestCode ==PICK_IMAGE){
+            imageUri = data.getData();
+//            imageView.setImageURI(imageUri);
+        }
+    }
+
+    private void openGallery() {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
     private void dispatchTakePictureIntent(){
         Intent takePictureIntent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if(takePictureIntent.resolveActivity(getPackageManager())!=null){
